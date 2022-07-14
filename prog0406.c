ce96ef4ba5090df497b859986ad77eca7a41eeaa
MatheusrCunha <matheus.rodriguesifsc@gmail.com>
2022-07-14 00:06:34 -0300

    prog0406

---
+++ prog0406.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+    int i = 1;
+
+    while(i<10)
+    {
+        printf("%d\n", i);
+        i += 1;
+    }
+
+    for(i=1;i<10;i++)
+    {
+        printf("\n\n%d\n", i);
+    }
+
+return 0;
+
+}
+
